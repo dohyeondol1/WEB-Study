4f86a1f043e65ae46e99ee28502b6a3fd54d60c6
dohyeondol <102894803+dohyeondol1@users.noreply.github.com>
2022-09-25 22:11:20 +0900

    26day
    function type

---
+++ 26day.js
@@ -0,0 +1,42 @@
+//함수선언식
+function gugudan(){
+  console.log("3 * 1 = 3");
+  console.log("3 * 2 = 6");
+  console.log("3 * 3 = 9");
+  console.log("3 * 4 = 12");
+  console.log("3 * 5 = 15");
+  console.log("3 * 6 = 18");
+  console.log("3 * 7 = 21");
+  console.log("3 * 8 = 24");
+  console.log("3 * 9 = 27");
+}
+gugudan();
+
+
+//함수표현식
+const gugudannn = function gugudan(){
+  console.log("3 * 1 = 3");
+  console.log("3 * 2 = 6");
+  console.log("3 * 3 = 9");
+  console.log("3 * 4 = 12");
+  console.log("3 * 5 = 15");
+  console.log("3 * 6 = 18");
+  console.log("3 * 7 = 21");
+  console.log("3 * 8 = 24");
+  console.log("3 * 9 = 27");
+};
+gugudannn();
+
+//익명함수
+const gugudan1 = function(){
+  console.log("3 * 1 = 3");
+  console.log("3 * 2 = 6");
+  console.log("3 * 3 = 9");
+  console.log("3 * 4 = 12");
+  console.log("3 * 5 = 15");
+  console.log("3 * 6 = 18");
+  console.log("3 * 7 = 21");
+  console.log("3 * 8 = 24");
+  console.log("3 * 9 = 27");
+};
+gugudan1();
