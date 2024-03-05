1d64dc20c28488ec964059ca87fe3038129080ed
Do Hyeon Seok <dohyeondol@gmail.com>
2024-03-06 01:05:11 +0900

    Delete 21day.js

--- 21day.js
+++
@@ -1,8 +0,0 @@
-let arr = [10];
-console.log(arr);
-
-let arr2 = [10, 20];
-console.log(arr2[1]); //INDEX
-
-let arr3 = [10, "abc", true, null, undefined, function(){}, {}]; //Symbol도 사용가능
-console.log(arr3[2]);
